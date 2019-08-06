bdb8028ca0cbae32adf5fa16ad4285287f4ad139
selmaraquel <53803258+selmaraquel@users.noreply.github.com>
2019-08-06 09:28:55 +0700

    add a comment

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ print('hello from Github')
 x = input('enter the first number: ')
 y = input('enter the second number: ')
 
+# function to do addition
 def add(first, second):
   print(int(first) + int(second))
  
